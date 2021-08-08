582aa32b8d2227bbd147e6bfe9ce219ffd3096f0
FuYH <1351330877@qq.com>
2021-08-08 20:29:27 +0800

    封装request

---
+++ src/utils/request.ts
@@ -0,0 +1,24 @@
+import Taro from '@tarojs/taro'
+
+export default function request(params) {
+  const baseURL = 'https://music.qier222.com/api'
+  // const baseURL = 'https://ping-music-api.vercel.app'
+  return new Promise((resolve, reject) => {
+    Taro.request({
+      url: baseURL + params.url,
+      method: params.method || 'get',
+      data: params.data || {},
+      success: res => {
+        resolve(res.data)
+      },
+      fail: err => {
+        Taro.showToast({
+          title: '网络异常',
+          icon: 'none',
+          duration: 1000
+        })
+        reject(err)
+      }
+    })
+  })
+}
