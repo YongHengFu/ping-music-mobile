0a803f0486389464a8d5ba7902447ad61a6bf254
FuYH <1351330877@qq.com>
2021-08-08 20:28:05 +0800

    引入Vuex

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
+import Vuex from '@/store'
+import '@tarojs/taro/html.css'
 import './app.scss'
 
 const App = createApp({
-  onShow (options) {},
-  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
-
+App.use(Vuex)
 export default App
---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { createStore } from 'vuex'
+const store = createStore({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  getters: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+
+  }
+})
+
+export default store
