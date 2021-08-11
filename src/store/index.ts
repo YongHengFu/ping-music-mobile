3ae61b9e62cfc4afff482d37bb9ad42eaabc1289
FuYH <1351330877@qq.com>
2021-08-11 23:03:22 +0800

    完善播放器

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,9 +3,6 @@ const store = createStore({
   state: {
     currIndex: 0,
     currTime: 0,
-    orderList: [],
-    randList: [],
-    mode: 0 // 0:列表循环 1：顺序播放 2：随机播放 3：单曲循环
   },
   mutations: {
     setCurrIndex(state, value) {
@@ -13,15 +10,6 @@ const store = createStore({
     },
     setCurrTime(state, value) {
       state.currTime = value
-    },
-    setOrderList(state, value) {
-      state.orderList = value
-    },
-    setRandList(state, value) {
-      state.randList = value
-    },
-    setMode(state, value) {
-      state.mode = value
     }
   },
   getters: {
--- src/utils/player.ts
+++ src/utils/player.ts
@@ -3,25 +3,66 @@ import Store from '@/store'
 
 const next = () => {
   return () => {
+    const musicList = Taro.getStorageSync('musicList')
+    if (musicList) {
+      let index = Store.state.currIndex
+      if (player.mode !== 3) {
+        if (index < musicList.length - 1) {
+          index++
+          Store.commit('setCurrIndex', index)
+        } else {
+          index = 0
+          Store.commit('setCurrIndex', index)
+        }
+      }
+      player.audio.src = `https://music.163.com/song/media/outer/url?id=${musicList[index].id}.mp3`
+    }
   }
 }
 const prev = () => {
   return () => {
+    const musicList = Taro.getStorageSync('musicList')
+    if (musicList) {
+      let index = Store.state.currIndex
+      if (player.mode !== 3) {
+        if (index > 0) {
+          index--
+          Store.commit('setCurrIndex', index)
+        } else {
+          index = musicList.length - 1
+          Store.commit('setCurrIndex', index)
+        }
+      }
+      player.audio.src = `https://music.163.com/song/media/outer/url?id=${musicList[index].id}.mp3`
+    }
+  }
+}
+
+const switchMode = () => {
+  return () => {
+    if (player.mode === 3) {
+      player.mode = 0
+    } else {
+      player.mode += 1
+    }
   }
 }
 
 const player = {
   audio: Taro.createInnerAudioContext(),
+  mode: 0,
+  randList: [],
   next: next(),
-  prev: prev()
+  prev: prev(), // 0:列表循环 1：顺序播放 2：随机播放 3：单曲循环
+  switchMode: switchMode()
 }
-// player.audio.autoplay = true
-player.audio.src = 'https://music.163.com/song/media/outer/url?id=1353301300.mp3'
+player.audio.autoplay = true
+// player.audio.src = 'https://music.163.com/song/media/outer/url?id=1353301300.mp3'
 player.audio.onTimeUpdate(() => {
   Store.commit('setCurrTime', player.audio.currentTime)
 })
+player.audio.onEnded(() => {
+  player.next()
+})
 
-setTimeout(() => {
-  // player.next()
-}, 10000)
 export default player
