cc116f07338065ed1e6f1692f78a1b3d1c461c62
FuYH <1351330877@qq.com>
2021-08-12 23:52:22 +0800

    完善播放控制条

--- src/components/ControlBar.vue
+++ src/components/ControlBar.vue
@@ -2,10 +2,15 @@
   <div :class="$style['control-bar']">
     <div :class="$style.bar">
       <img :src="image" :class="$style.cover">
-      <img :src="iconVinly" :class="$style.vinly">
-      <img v-if="state" :src="iconPause" :class="$style.pause" @click="pause">
-      <img v-else :src="iconPlay" :class="$style.play" @click="play">
+      <img :src="iconVinly" :class="[$style.vinly, {[$style['vinly-rotate']]:state}]">
+      <span :class="$style.text">{{ text }}</span>
+      <div :class="$style['right-icon']">
+        <img v-if="state" :src="iconPause" :class="$style.pause" @click="pause">
+        <img v-else :src="iconPlay" :class="$style.play" @click="play">
+        <img :src="iconMusicList" :class="$style.list" @click="showList=true">
+      </div>
     </div>
+    <MusicList v-if="showList" @close="showList=false" />
   </div>
 </template>
 
@@ -13,24 +18,37 @@
 import { computed, defineComponent, onMounted, ref, watch } from 'vue'
 import { useStore } from 'vuex'
 import player from '@/utils/player'
+import MusicList from '@/components/MusicList.vue'
 import iconVinly from '@/assets/icons/vinyl.png'
 import iconPlay from '@/assets/icons/play-mini.png'
 import iconPause from '@/assets/icons/pause-mini.png'
+import iconMusicList from '@/assets/icons/music-list.png'
 import Taro from '@tarojs/taro'
 export default defineComponent({
   name: 'ControlBar',
+  components: {
+    MusicList
+  },
   setup() {
     const store = useStore()
     const state = ref(false)
+    const showList = ref(false)
     const image = ref('')
+    const text = ref('PingMusic')
     const currIndex = computed(() => store.state.currIndex)
-    watch(currIndex, () => {
-      const musicList = Taro.getStorageSync('musicList')
-      if (musicList) {
-        image.value = musicList[currIndex.value].album.picUrl + '?param=800y800'
-        console.log(image.value)
+
+    watch(showList, () => {
+      if (showList.value) {
+        Taro.hideTabBar({
+          animation: true
+        })
+      } else {
+        Taro.showTabBar({
+          animation: true
+        })
       }
     })
+
     const play = () => {
       player.audio.play()
     }
@@ -44,13 +62,28 @@ export default defineComponent({
       player.audio.onPause(() => {
         state.value = false
       })
+      player.audio.onCanplay(() => {
+        const musicList = Taro.getStorageSync('musicList')
+        if (musicList) {
+          image.value = musicList[currIndex.value].album.picUrl + '?param=800y800'
+          text.value = musicList[currIndex.value].name + '-'
+          const artists = [] as string[]
+          for (const item of musicList[currIndex.value].artist) {
+            artists.push(item.name)
+          }
+          text.value += artists.join('/')
+        }
+      })
     })
     return {
       iconVinly,
       iconPlay,
       iconPause,
+      iconMusicList,
       state,
+      showList,
       image,
+      text,
       play,
       pause
     }
@@ -61,7 +94,7 @@ export default defineComponent({
 <style module lang="scss">
 .control-bar{
   position: fixed;
-  bottom: 10px;
+  bottom: 0;
   left: 30px;
   right: 30px;
   .bar{
@@ -72,26 +105,61 @@ export default defineComponent({
     align-items: center;
     position: relative;
     .cover{
-      width: 80px;
-      height: 80px;
+      width: 85px;
+      height: 85px;
       position: absolute;
       left: 0;
       bottom: 0;
-      box-shadow: 0 5px 5px #cccccc;
-      border-radius: 5px;
+      border-radius: 10px;
+      z-index: 2;
     }
     .vinly{
-      width: 60px;
+      min-width: 60px;
+      max-width: 60px;
       height: 60px;
-      margin-left: 45px;
+      transition: margin-left 0.5s linear;
+      margin-left: 40px;
     }
-    .play, .pause{
-      width: 35px;
-      height: 35px;
-      padding: 3px;
-      margin: 10px;
-      border: 4px solid #ffffff;
-      border-radius: 50%;
+    .vinly-rotate{
+      margin-left: 55px;
+      animation: rotate infinite 1s linear;
+      z-index: 1;
+    }
+    @keyframes rotate {
+      from{
+        transform: rotate(0);
+      }
+      to{
+        transform: rotate(360deg);
+      }
+    }
+    .text{
+      width: 100%;
+      padding: 20px;
+      font-size: 22px;
+      color: #fff;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .right-icon{
+      min-width: 130px;
+      max-width: 130px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      margin-right: 20px;
+      .play, .pause{
+        width: 35px;
+        height: 35px;
+        padding: 5px;
+        border: 4px solid #fff;
+        border-radius: 50%;
+      }
+      .list{
+        width: 40px;
+        height: 40px;
+      }
     }
   }
 }
